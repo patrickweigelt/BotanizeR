caf2e7871ea4642fc4bacec93b14f45a7b40dcce
Patrick Weigelt <weigelt.patrick@gmail.com>
2020-12-22 14:57:22 +0100

    added other hint options

--- R/BotanizeR_quiz.R
+++ R/BotanizeR_quiz.R
@@ -1,6 +1,6 @@
 ### BotanizeR
 
-BotanizeR_quiz <- function(species_list, hints = c("image","description","floristic status","conservation status","family","German name")){
+BotanizeR_quiz <- function(species_list, hints = c("image","description","status","habitat","family","German name")){
   
   # 1. Controls ----
   # Package dependencies
@@ -8,31 +8,35 @@ BotanizeR_quiz <- function(species_list, hints = c("image","description","floris
   require(XML)
   
   # Arguments
-  if(!all(hints %in% c("image","description","floristic status","conservation status","family","German name"))){
-    stop('"hints" must be a subset of c("image","description","floristic status","conservation status","family","German name")')
+  if(!all(hints %in% c("image","description","status","habitat","family","German name"))){
+    stop('"hints" must be a subset of c("image","description","status","habitat","family","German name")')
   }
   
-  # 2. Quiz ----
+  # Create folder for temp files
+  dir <- tempfile()
+  dir.create(dir)
+  
+  # Quiz ----
   i <- sample(1:nrow(species_list), 1, prob = species_list$SCORE/species_list$COUNT)
   
+  # Species i
   species <- species_list$SPECIES[i]
   
-  # Species i
-  
+
   # main infos
   # I need this step because of an error in RCURL when getting the url
-  download.file(paste("https://www.floraweb.de/pflanzenarten/artenhome.xsql?suchnr=",species_list$NAMNR[i],"&", sep=""), destfile = "main.txt", quiet = T)
-  html_main <- htmlTreeParse(file = "main.txt", isURL = F, isHTML=T, useInternalNodes = T)
+  download.file(paste("https://www.floraweb.de/pflanzenarten/artenhome.xsql?suchnr=",species_list$NAMNR[i],"&", sep=""), destfile = file.path(dir,"main.txt"), quiet = T)
+  html_main <- htmlTreeParse(file = file.path(dir,"main.txt"), isURL = F, isHTML=T, useInternalNodes = T)
   infos_main <- xpathApply(html_main, "//div[@id='content']//p",xmlValue)
   
   # ecology
-  download.file(paste("https://www.floraweb.de/pflanzenarten/oekologie.xsql?suchnr=",species_list$NAMNR[i],"&", sep=""), destfile = "ecology.txt", quiet = T)
-  html_ecology <- htmlTreeParse(file = "ecology.txt", isURL = F, isHTML=T, useInternalNodes = T)
+  download.file(paste("https://www.floraweb.de/pflanzenarten/oekologie.xsql?suchnr=",species_list$NAMNR[i],"&", sep=""), destfile = file.path(dir,"ecology.txt"), quiet = T)
+  html_ecology <- htmlTreeParse(file = file.path(dir,"ecology.txt"), isURL = F, isHTML=T, useInternalNodes = T)
   infos_ecology <- xpathApply(html_ecology, "//div[@id='content']//p",xmlValue)
   
   # photo
-  download.file(paste("https://www.floraweb.de/pflanzenarten/foto.xsql?suchnr=",species_list$NAMNR[i], sep=""), destfile = "photo.txt", quiet = T)
-  html_photo <- htmlTreeParse(file = "photo.txt", isURL = F, isHTML=T, useInternalNodes = T)
+  download.file(paste("https://www.floraweb.de/pflanzenarten/foto.xsql?suchnr=",species_list$NAMNR[i], sep=""), destfile = file.path(dir,"photo.txt"), quiet = T)
+  html_photo <- htmlTreeParse(file = file.path(dir,"photo.txt"), isURL = F, isHTML=T, useInternalNodes = T)
   infos_photo <- xpathApply(html_photo, "//div[@id='content']//p",xmlValue)
   photolink <- xpathApply(html_photo, "//div[@id='content']//img",xmlAttrs)[[1]][3]
   image=NA
@@ -40,10 +44,9 @@ BotanizeR_quiz <- function(species_list, hints = c("image","description","floris
     try(image <- load.image(paste("https://www.floraweb.de", gsub("\\.\\.","",photolink), sep="")),silent = T)
   }
   
-  
   attempts <- 0
   attempt <- "start"
-  while(attempt != species & attempt != "skip" & attempts <= 10){
+  while(attempt != species & attempt != "skip" & attempt != "noinfo" & attempts <= 10){
     
     for(k in 1:length(hints)){
       
@@ -69,8 +72,11 @@ BotanizeR_quiz <- function(species_list, hints = c("image","description","floris
           if(species==attempt | attempt == "skip" | attempts > 10){
             break()
           }
+        } else {
+          attempt <- "noinfo"
+          break()
         }
-      }
+      } 
       
       if(hints[k]=="description"){
         message(infos_photo[[which(infos_photo == "Bestimmungshilfe:")+1]])
@@ -91,12 +97,91 @@ BotanizeR_quiz <- function(species_list, hints = c("image","description","floris
           break()
         }
       }
+      if(hints[k]=="status"){
+        message(infos_main[[7]],"\n",infos_main[[8]])
+        
+        while(attempt != species & attempt != "" & attempt != "skip" & attempts <= 10){
+          attempts <- attempts + 1
+          
+          attempt <- readline("Species: ")
+          
+          if(attempt==""){
+            next()
+          }
+          if(species!=attempt & attempt != "skip"){        
+            message(adist(attempt, species)," characters different\n") 
+          }
+        }
+        if(species==attempt | attempt == "skip" | attempts > 10){
+          break()
+        }
+      }
+      if(hints[k]=="family"){
+        message(infos_main[[6]])
+        
+        while(attempt != species & attempt != "" & attempt != "skip" & attempts <= 10){
+          attempts <- attempts + 1
+          
+          attempt <- readline("Species: ")
+          
+          if(attempt==""){
+            next()
+          }
+          if(species!=attempt & attempt != "skip"){        
+            message(adist(attempt, species)," characters different\n") 
+          }
+        }
+        if(species==attempt | attempt == "skip" | attempts > 10){
+          break()
+        }
+      }
+      if(hints[k]=="German name"){
+        message(infos_main[[5]])
+        
+        while(attempt != species & attempt != "" & attempt != "skip" & attempts <= 10){
+          attempts <- attempts + 1
+          
+          attempt <- readline("Species: ")
+          
+          if(attempt==""){
+            next()
+          }
+          if(species!=attempt & attempt != "skip"){        
+            message(adist(attempt, species)," characters different\n") 
+          }
+        }
+        if(species==attempt | attempt == "skip" | attempts > 10){
+          break()
+        }
+      }
+      if(hints[k]=="habitat"){
+        message(infos_ecology[[3]])
+        
+        while(attempt != species & attempt != "" & attempt != "skip" & attempts <= 10){
+          attempts <- attempts + 1
+          
+          attempt <- readline("Species: ")
+          
+          if(attempt==""){
+            next()
+          }
+          if(species!=attempt & attempt != "skip"){        
+            message(adist(attempt, species)," characters different\n") 
+          }
+        }
+        if(species==attempt | attempt == "skip" | attempts > 10){
+          break()
+        }
+      }
     }
   }
   if(species==attempt){
-    message("Species correct after ",attempts,ifelse(attempts==1," attempt\n"," attempts\n"),infos_main[[4]],"\n",infos_main[[5]])
+    message("Species correct after ",attempts,ifelse(attempts==1," attempt\n"," attempts\n"),infos_main[[4]],"\n",infos_main[[5]],"\n\n")
+  }
+  if(attempt=="noinfo"){
+    message("No image for ",species, ".\n\n")
   } else {
-    message("Species not correct after ",attempts,ifelse(attempts==1," attempt\n"," attempts\n"),infos_main[[4]],"\n",infos_main[[5]])
+    message("Species not correct after ",attempts,ifelse(attempts==1," attempt\n"," attempts\n"),infos_main[[4]],"\n",infos_main[[5]],"\n\n")
   }
   BotanizeR_quiz(species_list, hints)
 }
