d61dfe35a408ed6a6904839b2195d675ea1d1c47
pierredenelle <pierre.denelle@gmail.com>
2021-01-11 17:30:55 +0100

    Shiny app updated: two pages, logo, some informations added

--- inst/server.R
+++ inst/server.R
@@ -22,17 +22,137 @@ shinyServer(function(input, output) {
     }
     
     # List of floraweb species
+    # observe({
     data(floraweb_species)
     floraweb_species <- floraweb_species[which(floraweb_species$SUMMER==1 |
                                                    floraweb_species$BioDiv2005==1), ]
     species_list <- floraweb_species
+    plant_list <- floraweb_species$SPECIES
     
     dir <- tempfile()
     dir.create(dir)
+    # })
     
+    # 1. Selected species ----
+    # Plant list
+    output$plant_list <- renderPrint({plant_list})
+    
+    selected_species <- reactive({
+        input$plant_list
+        selected_species
+    })
+    
+    # Simple text
+    # output$selected_sp <- renderPrint(input$plant_list)
+    
+    # Photo ----
+    output$selected_sp_photo <- renderPlot({
+        # Plant species chosen
+        j <- which(species_list$SPECIES == input$plant_list)
+
+        # Main infos
+        download.file(
+            paste0(
+                "https://www.floraweb.de/pflanzenarten/artenhome.xsql?suchnr=",
+                species_list$NAMNR[j], "&"),
+            destfile = file.path(dir, "main.txt"),
+            quiet = TRUE)
+        html_main <- htmlTreeParse(
+            file = file.path(dir, "main.txt"),
+            isURL = F, isHTML = T, useInternalNodes = T)
+        infos_main <- xpathApply(html_main, "//div[@id='content']//p",
+                                 xmlValue)
+        # Photo
+        image_select <- NA
+        
+        if(length(xpathApply(html_main, "//a[@class='imglink']"
+                             , xmlAttrs)) > 0 &
+           grepl("foto\\.xsql", xpathApply(html_main, "//a[@class='imglink']",
+                                           xmlAttrs))[1]){
+            download.file(
+                paste0("https://www.floraweb.de/pflanzenarten/",
+                       grep("foto\\.xsql",
+                            xpathApply(html_main, "//a[@class='imglink']",
+                                       xmlAttrs)[[1]], value = T)),
+                destfile = file.path(dir, "photo.txt"),
+                quiet = T)
+            html_photo <- htmlTreeParse(file = file.path(dir,"photo.txt"),
+                                        isURL = F, isHTML = T, useInternalNodes = T)
+            infos_photo <- xpathApply(html_photo, "//div[@id='content']//p", xmlValue)
+            # photolink <- xpathApply(html_photo, "//div[@id='content']//img",xmlAttrs)[[1]][3]
+            photolinks <- sapply(xpathApply(html_photo, "//div[@id='content']//img",
+                                            xmlAttrs),
+                                 function(x) grep("bilder", x, value = TRUE))
+            
+            if(photolinks[1] != "../bilder/arten/"){
+                try(image_select <- load.image(
+                    paste0("https://www.floraweb.de",
+                           gsub("\\.\\.", "", photolinks[1]))),
+                    silent = TRUE)
+            }
+        }
+        # Plot
+        par(mar = rep(0.5, 4), oma = rep(0, 4))
+        plot(image_select, axes = FALSE)
+        #, ylim = c(height(image_sp[[2]]), 1))
+    })
+    
+    # Description ----
+    output$selected_sp_description <- renderText({
+        # Plant species chosen
+        j <- which(species_list$SPECIES == input$plant_list)
+        print(j)
+        
+        download.file(paste0("https://www.floraweb.de/pflanzenarten/oekologie.xsql?suchnr=",
+                             species_list$NAMNR[j], "&"),
+                      destfile = file.path(dir, "ecology.txt"), quiet = T)
+        html_ecology <- htmlTreeParse(file = file.path(dir, "ecology.txt"),
+                                      isURL = F, isHTML=T, useInternalNodes = T)
+        infos_ecology <- xpathApply(html_ecology, "//div[@id='content']//p",
+                                    xmlValue)
+        
+        print(infos_ecology[[3]])
+    })
+    
+    # Status ----
+    output$selected_sp_status <- renderText({
+        # Plant species chosen
+        j <- which(species_list$SPECIES == input$plant_list)
+        print(j)
+        
+        download.file(paste0("https://www.floraweb.de/pflanzenarten/biologie.xsql?suchnr=",
+                             species_list$NAMNR[j], "&"),
+                      destfile = file.path(dir,"biology.txt"), quiet = T)
+        html_biology <- htmlTreeParse(file = file.path(dir,"biology.txt"), isURL = F, isHTML=T, useInternalNodes = T)
+        infos_biology <- xpathApply(html_biology, "//div[@id='content']//p",xmlValue)
+        
+        print(infos_biology[[2]])
+    })
+    
+    # Family ----
+    output$selected_sp_family <- renderPlot({
+        
+    })
+    
+    # Habitat ----
+    output$selected_sp_habitat <- renderPlot({
+        
+    })
+    
+    # German name ----
+    output$selected_sp_german <- renderPlot({
+        
+    })
+    
+    # Map ----
+    output$selected_sp_map <- renderPlot({
+        
+    })
+    
+    # 2. Quizz ----
     observe({
         input$newplant # hitting the new plant button
-        # Random sp ----
+        # random species
         species <- sample(species_list$SPECIES, 1)
         
         # species <- as.character(input$ex_sp) #species_list$SPECIES[i]
@@ -49,7 +169,8 @@ shinyServer(function(input, output) {
         html_main <- htmlTreeParse(# file = file.path("main.txt"),
             file = file.path(dir, "main.txt"),
             isURL = F, isHTML=T, useInternalNodes = T)
-        infos_main <- xpathApply(html_main, "//div[@id='content']//p",xmlValue)
+        infos_main <- xpathApply(html_main, "//div[@id='content']//p",
+                                 xmlValue)
         
         # Photo
         image <- NA
@@ -110,40 +231,44 @@ shinyServer(function(input, output) {
             # Defining & initializing the reactiveValues object
             counter <- reactiveValues(countervalue = 0) 
             output$nb_tries <- renderText({"Number of tries: 0"})
-            observeEvent(input$submit, {
-                counter$countervalue <- counter$countervalue + 1
-                output$nb_tries <- renderText({
-                    paste0("Number of tries: ", counter$countervalue)})
-                
-                # Initial score
-                counter <- reactiveValues(score = 0)
-                renderText("Score = 0")
-                # output$score <- 0
-                
-                # If provided answer is correct
-                # observeEvent(input$sp_answer == species, {
-                if (input$sp_answer == species){
-                    output$status1 <- renderText({
-                        ""
-                    })
-                    output$status2 <- renderText({
-                        paste(generateResponse(1))
-                    })
-                    output$status3 <- renderText({
-                        ""
-                    })
+            # observeEvent(input$submit, {
+            if(!is.null(input$lastkeypresscode)){
+                if(input$lastkeypresscode == 13){ # hitting Enter
+                    counter$countervalue <- counter$countervalue + 1
+                    output$nb_tries <- renderText({
+                        paste0("Number of tries: ", counter$countervalue)})
                     
-                    # Updating score
-                    renderText({
-                        counter$score <- counter$score + 1
-                        paste0("Score: ", counter$score)
-                        # output$score <- output$score + 1
-                        # paste0("Score: ", output$score)
+                    # Initial score
+                    counter <- reactiveValues(score = 0)
+                    renderText("Score = 0")
+                    # output$score <- 0
+                    
+                    # If provided answer is correct
+                    # observeEvent(input$sp_answer == species, {
+                    if (input$sp_answer == species){
+                        output$status1 <- renderText({
+                            ""
                         })
-                    
-                    # })
-                }
-            })
+                        output$status2 <- renderText({
+                            paste(generateResponse(1))
+                        })
+                        output$status3 <- renderText({
+                            ""
+                        })
+                        
+                        # Updating score
+                        renderText({
+                            counter$score <- counter$score + 1
+                            paste0("Score: ", counter$score)
+                            # output$score <- output$score + 1
+                            # paste0("Score: ", output$score)
+                        })
+                        
+                        # })
+                    }
+                } # closes 'Hitting enter'
+            } # closes !is.null lastkeybutton
+            # })
         })
         
         # Printing real answer ----
--- inst/ui.R
+++ inst/ui.R
@@ -1,4 +1,5 @@
 
+# Packages
 library(shiny)
 library(shinythemes)
 # library(shinyjs)
@@ -6,17 +7,42 @@ library(BotanizeR)
 library(imager)
 library(XML)
 
+# Data
+data(floraweb_species)
+floraweb_species <- floraweb_species[which(floraweb_species$SUMMER==1 |
+                                               floraweb_species$BioDiv2005==1), ]
+species_list <- floraweb_species
+plant_list <- floraweb_species$SPECIES
+
 # UI
 navbarPage(title = div(
-    HTML('<span style="font-size:120%;color:white;font-weight:bold;"> Botanic quizz&nbsp;&nbsp;</span></a>')
+    HTML('<span style="font-size:180%;color:white;font-weight:bold;"> BotanizeR</span></a>'),
+    # Team logo 
+    tags$script(HTML("var header = $('.navbar > .container-fluid');
+             header.append('<div style=\"float:right\"><a href=\"https://www.uni-goettingen.de/en/128741.html\"><img src=\"biodiv_gottingen_logo.png\" alt=\"alt\" style=\"float:right; width:140px;height:80px;padding-top:10px;\"> </a></div>');console.log(header)"))
 ),
 theme = shinytheme("flatly"),
 windowTitle = "Botanic quizz",
 
-## Main page ------------------------------------------------------------------
+## Species ---------------------------------------------------------
+tabPanel(titlePanel("Species info"),
+         fluidRow(column(4,
+                         selectInput("plant_list", "Plant list",
+                                     choices = plant_list,
+                                     # selectize = FALSE,
+                                     selected = "Acer campestre")),
+                  column(8,
+                         plotOutput("selected_sp_photo")
+                         ),
+                  column(4,
+                         textOutput("selected_sp_description"),
+                         br(),
+                         textOutput("selected_sp_status")))),
+
+## Quizz ----------------------------------------------------------------------
 tabPanel(
     # Application title
-    titlePanel("Main page"),
+    titlePanel("Quizz"),
     fluidRow(
         column(4,
                h5(textOutput("Score")),
@@ -26,6 +52,10 @@ tabPanel(
                                                  "Family", "Habitat",
                                                  "German name", "Map")),
                br(),
+               tags$script(' $(document).on("keydown", function (e) {
+                                                  Shiny.onInputChange("lastkeypresscode", e.keyCode);
+                                                  });
+                                                  '),
                textInput("sp_answer", label = "Species name"),
                h5(textOutput("nb_tries"), style = "font-weight=500; color: #000000;"),
                br(),
@@ -37,7 +67,8 @@ tabPanel(
                br(),
                actionButton("submit", "Submit"),
                actionButton("real_answer", "Answer"),
-               actionButton("newplant", "New plant")),
+               actionButton("newplant", "New plant")
+               ),
         
         # Second part of the page with the picture
         column(8, plotOutput("random_sp"),
