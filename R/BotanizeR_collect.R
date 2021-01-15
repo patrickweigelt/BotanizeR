02b38ca6f4062689758f2e9d89e24fcee8194b89
Patrick Weigelt <weigelt.patrick@gmail.com>
2021-01-15 16:08:47 +0100

    changed back description part

--- R/BotanizeR_collect.R
+++ R/BotanizeR_collect.R
@@ -47,17 +47,22 @@ BotanizeR_collect <- function(species_row, image_floraweb=TRUE, hints_floraweb =
     
     # Main infos
     # I need this step because of an error in RCURL when getting the url
-    download.file(paste("https://www.floraweb.de/pflanzenarten/artenhome.xsql?suchnr=",species_row$NAMNR,"&", sep=""), destfile = file.path(dir,"main.txt"), quiet = T)
+    download.file(paste("https://www.floraweb.de/pflanzenarten/artenhome.xsql?suchnr=",species_row$NAMNR,"&", sep=""), 
+                  destfile = file.path(dir,"main.txt"), quiet = T)
     html_main <- htmlTreeParse(file = file.path(dir,"main.txt"), isURL = F, isHTML=T, useInternalNodes = T)
     infos_main <- xpathApply(html_main, "//div[@id='content']//p",xmlValue)
     
     if(image_floraweb){
       
-      #download.file(paste("https://www.floraweb.de/pflanzenarten/foto.xsql?suchnr=",species_row$NAMNR[i], sep=""), destfile = file.path(dir,"photo.txt"), quiet = T)
+      #download.file(paste("https://www.floraweb.de/pflanzenarten/foto.xsql?suchnr=",species_row$NAMNR[i], sep=""),
+      #                    destfile = file.path(dir,"photo.txt"), quiet = T)
       
       # Photo
-      if(length(xpathApply(html_main, "//a[@class='imglink']",xmlAttrs))>0 & grepl("foto\\.xsql",xpathApply(html_main, "//a[@class='imglink']",xmlAttrs))[1]){
-        download.file(paste("https://www.floraweb.de/pflanzenarten/",grep("foto\\.xsql",xpathApply(html_main, "//a[@class='imglink']",xmlAttrs)[[1]], value = T), sep=""), destfile = file.path(dir,"photo.txt"), quiet = T)
+      if(length(xpathApply(html_main, "//a[@class='imglink']",xmlAttrs))>0 & 
+         grepl("foto\\.xsql",xpathApply(html_main, "//a[@class='imglink']",xmlAttrs))[1]){
+        
+        download.file(paste("https://www.floraweb.de/pflanzenarten/",grep("foto\\.xsql",xpathApply(html_main, "//a[@class='imglink']",xmlAttrs)[[1]], value = T), sep=""), 
+                      destfile = file.path(dir,"photo.txt"), quiet = T)
         html_photo <- htmlTreeParse(file = file.path(dir,"photo.txt"), isURL = F, isHTML=T, useInternalNodes = T)
         infos_photo <- xpathApply(html_photo, "//div[@id='content']//p",xmlValue)
         # photolink <- xpathApply(html_photo, "//div[@id='content']//img",xmlAttrs)[[1]][3]
@@ -133,7 +138,8 @@ BotanizeR_collect <- function(species_row, image_floraweb=TRUE, hints_floraweb =
       
       # ecology
       if("habitat" %in% hints_floraweb){
-        download.file(paste("https://www.floraweb.de/pflanzenarten/oekologie.xsql?suchnr=",species_row$NAMNR,"&", sep=""), destfile = file.path(dir,"ecology.txt"), quiet = T)
+        download.file(paste0("https://www.floraweb.de/pflanzenarten/oekologie.xsql?suchnr=",species_row$NAMNR,"&"), 
+                      destfile = file.path(dir,"ecology.txt"), quiet = T)
         html_ecology <- htmlTreeParse(file = file.path(dir,"ecology.txt"), isURL = F, isHTML=T, useInternalNodes = T)
         infos_ecology <- xpathApply(html_ecology, "//div[@id='content']//p",xmlValue)
       }
@@ -151,7 +157,8 @@ BotanizeR_collect <- function(species_row, image_floraweb=TRUE, hints_floraweb =
       
       # map
       map <- NA
-      if("map" %in% hints_floraweb & length(xpathApply(html_main, "//a[@class='imglink']",xmlAttrs))>0 & any(grepl("webkarten",xpathApply(html_main, "//a[@class='imglink']",xmlAttrs)))){
+      if("map" %in% hints_floraweb & length(xpathApply(html_main, "//a[@class='imglink']",xmlAttrs))>0 & 
+         any(grepl("webkarten",xpathApply(html_main, "//a[@class='imglink']",xmlAttrs)))){
         
         if(!exists("CGRS_Germany")){
           data(CGRS_Germany)
@@ -179,9 +186,9 @@ BotanizeR_collect <- function(species_row, image_floraweb=TRUE, hints_floraweb =
           map$AFE_SYMBOLCODE[is.na(map$AFE_SYMBOLCODE)] <- 1
           map$AFE_SYMBOLCODE <- as.factor(map$AFE_SYMBOLCODE)
           
-          legend_info <- data.frame(AFE_SYMBOLCODE=c(1:8), SYMBOL_TEXT=c("absent",
-                                                                         "not assigned","records uncertain","extinct","probably extinct",
-                                                                         "cultivated, not established alien","established alien","native, incl. archaeophytes"),
+          legend_info <- data.frame(AFE_SYMBOLCODE=c(1:8), 
+                                    SYMBOL_TEXT=c("absent","not assigned","records uncertain","extinct","probably extinct",
+                                                  "cultivated, not established alien","established alien","native, incl. archaeophytes"),
                                     colour=c("white","grey90","grey80","grey70","grey60","#fdb462","#fb8072","#b3de69")) 
           
           legend_info <- legend_info[which(legend_info$AFE_SYMBOLCODE %in% map$AFE_SYMBOLCODE),]
@@ -194,21 +201,12 @@ BotanizeR_collect <- function(species_row, image_floraweb=TRUE, hints_floraweb =
         
         if(hints_floraweb[i] == "description"){
           if(floraweb_image){
-            description <- list(
-              species_row$TAXONNAME,
-              paste0("Bestimmungshilfe/Morphologie:\n",
-              infos_photo[[which(infos_photo == "Bestimmungshilfe:") + 1]]))
-            
-            # description <- paste0(
-            #   species_row$TAXONNAME, "\n",
-            #   "Bestimmungshilfe/Morphologie:\n",
-            #   infos_photo[[which(infos_photo == "Bestimmungshilfe:") + 1]])
-          } else{
-            description <- list(c(""),
-                                gsub("Morphologie:", "Morphologie:\n",
-                                     infos_biology[[2]]))
+            description <- paste0("Bestimmungshilfe/Morphologie:\n",
+                                  infos_photo[[which(infos_photo == "Bestimmungshilfe:")+1]])
+          } else {
+            description <- gsub("Morphologie:","Morphologie:\n",infos_biology[[2]])
           }
-          if(!grepl("keine Angaben", description[[2]])){
+          if(!grepl("keine Angaben", description)){
             hints[[i+1]] <- description
           } else{
             hints[[i+1]] <- list(species_row$TAXONNAME, "")
