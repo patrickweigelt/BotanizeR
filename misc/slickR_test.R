2feb83d60e44bcf32fc4e0dd5d5dcaf462d94fb0
Patrick Weigelt <weigelt.patrick@gmail.com>
2021-01-16 13:35:02 +0100

    slickR test

---
+++ misc/slickR_test.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(slickR)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      ####
+    ),
+    
+    mainPanel(
+      slickROutput("slickr", width="500px")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$slickr <- renderSlickR({
+    # imgs <- list.files("inst/www/pictures_Clemens/", pattern="Acer campestre", recursive=TRUE, full.names = TRUE)
+    imgs <- list.files("inst/www/pictures_Clemens/", pattern="\\.png", recursive=TRUE, full.names = TRUE)
+    slickR(imgs)
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
