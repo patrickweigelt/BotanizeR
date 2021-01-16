571201515b58ac5674e4283c37f32ce254b87952
Patrick Weigelt <weigelt.patrick@gmail.com>
2021-01-16 14:14:07 +0100

    trial

--- misc/slickR_test.R
+++ misc/slickR_test.R
@@ -18,7 +18,7 @@ server <- function(input, output) {
   output$slickr <- renderSlickR({
     # imgs <- list.files("inst/www/pictures_Clemens/", pattern="Acer platanoides", recursive=TRUE, full.names = TRUE)
     imgs <- list.files("inst/www/pictures_Clemens/", pattern="\\.png", recursive=TRUE, full.names = TRUE)
-    imgs <- slick_list(slick_div(imgs, css = htmltools::css(width = "500px"),type = "img",links = NULL))
+    # imgs <- slick_list(slick_div(imgs, css = htmltools::css(width = "500px"),type = "img",links = NULL))
     slickR(imgs)
   })
 }
