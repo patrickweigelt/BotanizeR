d40f76caef8357066d1f1b652260c1df3558c3ef
unknown <pierre.denelle@gmail.com>
2021-06-29 16:10:35 +0200

    BotanizeR_species: R function and doc created

---
+++ R/BotanizeR_species.R
@@ -0,0 +1,20 @@
+#'
+#' Species table for BotanizeR
+#'
+#' Table containing the relevant information for 5852 species. The data
+#' originate from [FloraWeb](https://www.floraweb.de).
+#'
+#' @docType data
+#'
+#' @usage data("BotanizeR_species")
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://www.floraweb.de}{FloraWeb}
+#'
+#' @examples
+#' data(BotanizeR_species)
+"BotanizeR_species"
