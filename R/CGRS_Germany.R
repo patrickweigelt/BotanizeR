8ad8622d4d26526b8159ee1a9d8493aadd47f8a8
unknown <pierre.denelle@gmail.com>
2021-06-29 16:33:29 +0200

    CGRS_Germany: R and man

---
+++ R/CGRS_Germany.R
@@ -0,0 +1,18 @@
+#'
+#' Species table for BotanizeR
+#'
+#' Chorological Grid Reference System for Germany.
+#'
+#' @docType data
+#'
+#' @usage data("CGRS_Germany")
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://www.eea.europa.eu/data-and-maps/data/common-european-chorological-grid-reference-system-cgrs}{Common European Chorological Grid Reference System (CGRS)}
+#'
+#' @examples
+#' data(CGRS_Germany)
+"CGRS_Germany"
